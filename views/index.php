1ecc3d5d4d5a175aa0cb9e6f3cd3fe9c6dcec081
Szczepan <szczep.kowalik@gmail.com>
2021-12-02 08:50:29 +0100

    added views template files, static with .css

---
+++ views/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home Page</title>
+</head>
+<body>
+    <header>
+         <h1> Welcome </h1>
+
+    </header>
+    <main>
+        <div class="input_form">
+            <form action="#" method="POST">
+                <label for="file"><h3> Insert a .csv file: </h3></label>
+                <input type="file" name="file">
+                <button> Send you file </button>
+            </form>
+        </div>
+    </main>
+    <footer>
+        Made by Me
+    </footer>
+</body>
+</html>
